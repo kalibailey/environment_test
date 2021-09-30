37bed893827187ef4ba53f45576cb4d00e54e63e
root <root@localhost>
2021-09-30 14:02:12 +0000

    Book Collection Almost Complete

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: 6.95
+    fill_in 'Publish date', with: '2010-10-10'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('J. K. Rowling')
+    expect(page).to have_content(6.95)
+    expect(page).to have_content('2010-10-10')
   end
+
+    
 end
